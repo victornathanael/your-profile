e44e01524393ed1b1833decb3031e2177a7d672f
Victor Nathanael <victornathanael15@gmail.com>
2023-04-28 10:19:23 -0300

    feat: add SearchIcon and style for input

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,31 +1,50 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
+import SearchIcon from 'react-native-vector-icons/Feather';
 
 export default function Header() {
     return (
         <View style={styles.headerContainer}>
-            <TextInput
-                style={styles.input}
-                placeholder='Buscar capacitações, profissionais...'
-            />
+            <View style={styles.inputContainer}>
+                <SearchIcon
+                    name='search'
+                    size={22}
+                    style={styles.inputIcon}
+                ></SearchIcon>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Buscar capacitações, profissionais...'
+                    placeholderTextColor='black'
+                />
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#006557',
         width: '100%',
-        height: '35%',
+        height: '20%',
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        width: '90%',
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 20,
     },
     input: {
-        alignSelf: 'center',
-        height: 50,
-        width: '80%',
-        margin: 12,
-        marginTop: 50,
-        padding: 10,
-        borderRadius: 10,
+        flex: 1,
+        fontFamily: 'Montserrat_500Medium',
+        fontSize: 16,
+        padding: 0,
         backgroundColor: 'white',
+        marginLeft: 10,
     },
 });
