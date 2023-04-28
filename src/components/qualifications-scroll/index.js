3c2041766e37b3ddb8e459197a7b81750b481308
Victor Nathanael <victornathanael15@gmail.com>
2023-04-28 10:27:18 -0300

    feat: add qualifications data and QualificationsScroll component

---
+++ src/components/qualifications-scroll/index.js
@@ -0,0 +1,54 @@
+import { ScrollView, FlatList } from 'react-native';
+import QualificationCard from '../qualification-card';
+
+const data = [
+    {
+        id: '1',
+        title: 'Parceiros',
+        color: '#d48a04',
+        image: require('../../../assets/your-profile/alimentos.png'),
+        description: 'Diabetes: alimentos para evitar',
+    },
+    {
+        id: '2',
+        title: 'Capacitações',
+        color: '#3b2d72',
+        image: require('../../../assets/your-profile/remedio.png'),
+        description: 'Como ministrar doses',
+    },
+    {
+        id: '3',
+        title: 'Exercícios em casa',
+        color: '#a0c238',
+        image: require('../../../assets/your-profile/exercicio-em-casa.png'),
+        description: 'Descubra exercícios simples que podem ser feitos em casa',
+    },
+    {
+        id: '4',
+        title: 'Cuidados com a pele',
+        color: '#ffa07a',
+
+        image: require('../../../assets/your-profile/cuidados-com-a-pele.png'),
+        description: 'Dicas para manter a pele saudável e jovial',
+    },
+];
+
+export default function QualificationsScroll() {
+    return (
+        <ScrollView horizontal>
+            <FlatList
+                data={data}
+                renderItem={({ item }) => (
+                    <QualificationCard
+                        title={item.title}
+                        image={item.image}
+                        description={item.description}
+                        color={item.color}
+                    />
+                )}
+                keyExtractor={(item) => item.id}
+                numColumns={data.length}
+            />
+        </ScrollView>
+    );
+}
