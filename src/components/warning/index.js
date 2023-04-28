7141d27070a181103ecf450886455b42b13a5e89
Victor Nathanael <victornathanael15@gmail.com>
2023-04-28 10:30:12 -0300

    feat: add Warning component

---
+++ src/components/warning/index.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from 'react-native';
+import WarningIcon from 'react-native-vector-icons/AntDesign';
+
+export default function Warning() {
+    return (
+        <View style={styles.warningContainer}>
+            <WarningIcon
+                style={{ padding: 5, paddingHorizontal: 15 }}
+                name='warning'
+                size={25}
+                color='#8b6421'
+            ></WarningIcon>
+            <Text style={styles.warningText}>
+                <Text style={{ fontFamily: 'Montserrat_600SemiBold' }}>
+                    Completar perfil:{' '}
+                </Text>
+                Preencha suas informações para encontrar pacientes.
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    warningContainer: {
+        alignItems: 'center',
+        marginLeft: 20,
+        width: '90%',
+        flexDirection: 'row',
+        marginTop: 30,
+        backgroundColor: '#f8f4c4',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#d6bb8e',
+        paddingVertical: 10,
+    },
+    warningText: {
+        flexWrap: 'wrap',
+        width: '90%',
+        fontFamily: 'Montserrat_500Medium',
+        fontSize: 16,
+        color: '#987635',
+        paddingRight: 10,
+    },
+});
