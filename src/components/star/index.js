c95d9253ad93e992b70f4d4ce5b0b646a4de4627
Victor Nathanael <victornathanael15@gmail.com>
2023-04-28 10:29:07 -0300

    feat: add Star component

---
+++ src/components/star/index.js
@@ -0,0 +1,12 @@
+import StarIcon from 'react-native-vector-icons/AntDesign';
+
+export default function Star() {
+    return (
+        <StarIcon
+            name='star'
+            color='#d3d3d3'
+            size={18}
+            style={{ marginRight: 3 }}
+        ></StarIcon>
+    );
+}
